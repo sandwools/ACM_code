c12351cb47de3c98b0663aabde2f9836c48e670e
sandwools <arktis@foxmail.com>
2022-04-09 15:50:19 +0800

    ac c

--- C.cpp
+++ C.cpp
@@ -4,31 +4,77 @@
 using namespace std;
 
 char map[8][8]={0};
-int _map,_n;
-int realMap[8][8]={0};
+int map_len,_n;
 int foot[8][8]={0};
+int canMap[8]={0};
+int res=0;
+
+void dfs(int num,int cnt)
+{
+    if(cnt==_n)
+       {
+           res++;
+           return;
+       }
+
+    if(num==map_len)return;
+
+    for(int i=0;i<map_len;i++)
+    {
+        if(map[num][i]=='#'&&!canMap[i])
+        {
+            canMap[i]=1;
+            dfs(num+1,cnt+1);
+            canMap[i]=0;
+        }
+    }
+    dfs(num+1,cnt);
+}
+
+int sol(int need)
+{
+    int k=0;
+    if(need==0){return 1;}
+
+    {
+        for(int i=0;i<map_len;i++)
+           for(int j=0;j<map_len;j++)
+           {
+               if(canMap[i]==0)
+               {canMap[i]==0;k+=sol(need-1);}
+           }
+    }
+
+    return k;
+}
 
 int main()
 {
-    cin>>_map>>_n;
-    for(int i=0;i<_map;i++)
+    while(1)
+    
     {
-        for(int j=0;j<_map;j++)
+    cin>>map_len>>_n;
+    if(map_len==-1){system("pause");return 0;}
+    for(int i=0;i<map_len;i++)
+    {
+        for(int j=0;j<map_len;j++)
         {
             cin>>map[i][j];
         }
     }
 
-    for(int i=0;i<_map;i++)
-    {
-        for(int j=0;j<_map;j++)
-        {
-            if(map[i][j]='#'){realMap[i][j]=1;}
-            if(map[i][j]='.'){realMap[i][j]=1;}
-        }
-    }
+    sol(_n);
 
-    //DFS:
+    dfs(0,0);
 
     
+
+    cout<<res<<endl;
+    char map[8][8]={0};
+    foot[8][8]={0};
+    canMap[8]={0};
+    res=0;
+    }
+    system("pause");
+
 }
